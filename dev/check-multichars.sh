12f911ea0348c8d39e31046bb8d31f1990931b2d
Francis Tyers <ftyers@prompsit.com>
2018-07-09 16:56:59 +0200

    add multichar checking script

---
+++ dev/check-multichars.sh
@@ -0,0 +1,9 @@
+
+head=`grep -nH 'LEXICON Root' apertium-chv.chv.lexc  | cut -f2 -d':'`
+
+for tag in `cat apertium-chv.chv.lexc | grep -o '%<[^>]\+%>' | sort -u`;  do
+	found=`head -n $head apertium-chv.chv.lexc | grep "$tag" | wc -l`
+	if [[ $found -eq 0 ]]; then
+		echo "$tag                         !";
+	fi
+done
