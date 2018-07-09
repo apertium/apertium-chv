6b63aabccad70912aa426675949dc3c75b0eac73
Francis Tyers <ftyers@prompsit.com>
2018-07-09 16:58:12 +0200

    add multichar checking script

--- dev/check-multichars.sh
+++ dev/check-multichars.sh
@@ -7,3 +7,10 @@ for tag in `cat apertium-chv.chv.lexc | grep -o '%<[^>]\+%>' | sort -u`;  do
 		echo "$tag                         !";
 	fi
 done
+
+for tag in `cat apertium-chv.chv.lexc | grep -o '%{[^}]\+%}' | sort -u`;  do
+	found=`head -n $head apertium-chv.chv.lexc | grep "$tag" | wc -l`
+	if [[ $found -eq 0 ]]; then
+		echo "$tag                         !";
+	fi
+done
